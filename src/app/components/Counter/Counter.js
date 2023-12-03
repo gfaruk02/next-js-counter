408c6f50ba92c8a271992d3651169ade431facd5
gfaruk02 <gfaruk02@gmail.com>
2023-12-03 12:50:56 +0600

    next js counter

---
+++ src/app/components/Counter/Counter.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+const Counter = () => {
+    const [counter, setCounter]= useState(0)
+    return (
+        <div>
+                  <h1> Counter: {counter}</h1>
+     <button className="btn btn-primary pr-5" onClick={()=> setCounter(counter+1)}> Increase</button>
+     <button className="btn btn-primary" onClick={()=> setCounter(counter-1)}> Decrease</button>
+        </div>
+    );
+};
+
+export default Counter;
